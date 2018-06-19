c2eeb7af2109468c05d657ae82ff3550de5feb00
Katerina <kat6123@yandex.ru>
2018-06-19 13:19:40 +0300

    Add stubs for handlers.

---
+++ handlers/handler.go
@@ -0,0 +1,27 @@
+package handlers
+
+import "net/http"
+
+func Take(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Fund(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func AnnounceTournament(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func JoinTournament(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func ResultTournament(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Balance(w http.ResponseWriter, r *http.Request) {
+
+}
