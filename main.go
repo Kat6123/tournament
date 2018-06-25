6eec21f93081a0fbd425c8f71d15471212fda999
Katerina <kat6123@yandex.ru>
2018-06-25 17:22:50 +0300

    Add using of db builder to main.

--- main.go
+++ main.go
@@ -4,13 +4,21 @@ import (
 	"log"
 	"net/http"
 
+	"fmt"
+
 	"github.com/kat6123/tournament/db"
 	"github.com/kat6123/tournament/handler"
+	"github.com/kat6123/tournament/logic"
 )
 
 func main() {
-	db.Connect()
-	defer db.Close()
+	repository := db.New(db.Builder{
+		URL: "127.0.0.1:27017",
+	})
+	if err := repository.Connect(); err != nil {
+		panic(fmt.Errorf("dial with db has failed: %v", err))
+	}
+	defer repository.Close()
 
 	log.Fatal(http.ListenAndServe(":8080", handler.ServeRoutes()))
 }
