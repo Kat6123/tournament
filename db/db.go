0edc0752c85bab416df9a7e8ed869714da2b487c
Katerina <kat6123@yandex.ru>
2018-06-27 12:49:44 +0300

    Restructure db package add structs which contains collections.

--- db/db.go
+++ db/db.go
@@ -2,90 +2,31 @@ package db
 
 import (
 	"github.com/globalsign/mgo"
-	"github.com/kat6123/tournament/api"
-	"github.com/kat6123/tournament/model"
 )
 
-type mgoSession struct {
-	url     string
-	session *mgo.Session
-}
-
-// Builder type should be used to init connection to MongoDB.
-type Builder struct {
-	URL string
-}
-
-// New returns value of api.Repository interface type which was tuned with Builder.
-func New(b Builder) api.Repository {
-	return &mgoSession{
-		url: b.URL,
+type (
+	// Players implements PlayerProvider interface.
+	Players struct {
+		*mgo.Collection
 	}
-}
 
-func (m *mgoSession) Connect() (err error) {
-	m.session, err = mgo.Dial(m.url)
-	return err
-}
+	// Tournaments implements TournamentProvider interface and.
+	Tournaments struct {
+		*mgo.Collection
+	}
+)
 
-func (m *mgoSession) Close() {
-	// Close doesn't return error ?
-	m.session.Close()
-}
-
-func (m *mgoSession) LoadPlayer(playerID int) (*model.Player, error) {
-	session := m.session.Copy()
-	defer session.Close()
-
-	c := session.DB("tournament").C("players")
-	var p model.Player
-
-	err := c.FindId(playerID).One(&p)
+// New dials with db and init Players and Tournaments with Mongo collections,
+// which are named 'players' and 'tours' accordingly.
+func New(URL string, DB string) (*Players, *Tournaments, error) {
+	s, err := mgo.Dial(URL)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	return &p, nil
+	return &Players{s.DB(DB).C("players")},
+		&Tournaments{s.DB(DB).C("tours")},
+		nil
 }
 
-func (m *mgoSession) SavePlayer(p *model.Player) error {
-	s := m.session.Copy()
-	defer s.Close()
-
-	c := s.DB("tournament").C("players")
-
-	return c.UpdateId(p.ID, &p)
-}
-
-func (m *mgoSession) LoadTournament(tourID int) (*model.Tournament, error) {
-	s := m.session.Copy()
-	defer s.Close()
-
-	c := s.DB("tournament").C("tours")
-	var t model.Tournament
-
-	err := c.FindId(tourID).One(&t)
-	if err != nil {
-		return nil, err
-	}
-
-	return &t, nil
-}
-
-func (m *mgoSession) SaveTournament(t *model.Tournament) error {
-	s := m.session.Copy()
-	defer s.Close()
-
-	c := s.DB("tournament").C("tours")
-
-	return c.UpdateId(t.ID, &t)
-}
-
-func (m *mgoSession) CreateTournament(t *model.Tournament) error {
-	s := m.session.Copy()
-	defer s.Close()
-
-	c := s.DB("tournament").C("tours")
-
-	return c.Insert(t)
-}
+// How to close the session?
---
+++ db/tour.go
@@ -0,0 +1,18 @@
+package db
+
+import "github.com/kat6123/tournament/model"
+
+func (tc *Tournaments) LoadTournament(tourID int) (*model.Tournament, error) {
+	t := new(model.Tournament)
+	err := tc.FindId(tourID).One(t)
+
+	return t, err
+}
+
+func (tc *Tournaments) SaveTournament(t *model.Tournament) error {
+	return tc.UpdateId(t.ID, t)
+}
+
+func (tc *Tournaments) CreateTournament(t *model.Tournament) error {
+	return tc.Insert(t)
+}
