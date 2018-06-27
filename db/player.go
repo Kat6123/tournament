08c432ac9d6119e9163085c65832cdb1d14b7dd9
Katerina <kat6123@yandex.ru>
2018-06-27 14:42:03 +0300

    Restructure db package add structs which contains collections.

---
+++ db/player.go
@@ -0,0 +1,15 @@
+package db
+
+import "github.com/kat6123/tournament/model"
+
+// Player will be inited even if err
+func (pc *Players) LoadPlayer(playerID int) (*model.Player, error) {
+	p := new(model.Player)
+	err := pc.FindId(playerID).One(p)
+
+	return p, err
+}
+
+func (pc *Players) SavePlayer(p *model.Player) error {
+	return pc.UpdateId(p.ID, &p)
+}
