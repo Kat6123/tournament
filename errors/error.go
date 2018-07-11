36a1d7254425c13d44b2613bd0a089068eab0014
Katerina <kat6123@yandex.ru>
2018-07-11 14:22:22 +0300

    Refactor errors package.

--- errors/error.go
+++ errors/error.go
@@ -6,38 +6,57 @@ import (
 )
 
 const (
-	NotFound kind = iota
+	// NotFound error.
+	NotFound Kind = iota
+	// Duplicate error.
 	Duplicate
+	// Other is the kind of not classified error.
 	Other
 )
 
 type (
-	kind   int
-	entity string
+	// Kind of error.
+	Kind int
 
-	Error struct {
+	// TourError is a special type of error which is used by tour microservice.
+	TourError struct {
 		ID     int
-		Kind   kind
-		Entity entity
+		Kind   Kind
+		Entity string
 		Err    error
 	}
 )
 
-func (e *Error) Error() string {
+func (e *TourError) Error() string {
 	return fmt.Sprintf("%s %d: %s", e.Entity, e.ID, e.Err)
 }
 
-func (e *Error) wrap(s string) {
-	e.Err = fmt.Errorf("%s: %v", s, e.Err)
+// Wrap return new error which was wrapped by s string.
+// If e is of type TourError then Err field will be wrapped.
+func Wrap(e error, s string) error {
+	t, ok := e.(*TourError)
+	if ok {
+		t.Err = fmt.Errorf("%s: %v", s, t.Err)
+		return t
+	}
+
+	return fmt.Errorf("%s: %v", s, e)
+
 }
 
-func (e *Error) httpStatus() int {
-	switch e.Kind {
-	case NotFound:
-		return http.StatusNotFound
-	case Duplicate:
-		return http.StatusBadRequest
-	default:
-		return http.StatusInternalServerError
+// HTTPStatus return status based on error type.
+func HTTPStatus(e error) int {
+	t, ok := e.(*TourError)
+	if ok {
+		switch t.Kind {
+		case NotFound:
+			return http.StatusNotFound
+		case Duplicate:
+			return http.StatusBadRequest
+		default:
+			return http.StatusInternalServerError
+		}
 	}
+
+	return http.StatusInternalServerError
 }
